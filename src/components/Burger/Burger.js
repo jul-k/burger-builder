c9ccdbda67697daf203120de0b2dcda72921a374
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2018-01-05 14:06:46 -0500

    Calculating the quontity of ingridients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import BurgerIngridient from "./BurgerIngridient/BurgerIngridient";
 import classes from "./Burger.css";
 
 const burger = (props) => {
-    const transformedIngridients = Object.keys(props.ingridients)
+    let transformedIngridients = Object.keys(props.ingridients)
         .map(ingKey => {
             return [...Array(props.ingridients[ingKey])].map((_, index)=> {
                 return <BurgerIngridient key={ingKey + index} type={ingKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    console.log(transformedIngridients);
+
+    if (transformedIngridients.length === 0) {
+        transformedIngridients = <p>Please, start adding ingridients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
