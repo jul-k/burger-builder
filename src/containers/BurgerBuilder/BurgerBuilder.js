04dd4ffdef52dbd08bcee40471d0afd85d017a41
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2018-01-06 13:41:49 -0500

    Added the Order btn

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,22 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(ingKey => {
+                return ingredients[ingKey]
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        
+        this.setState({
+            purchasable: sum > 0
+        });
     }
 
     addIngredientHandler = (type) => {
@@ -37,6 +52,8 @@ class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingridients: updatedIngredients
         });
+
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -60,6 +77,8 @@ class BurgerBuilder extends Component {
             totalPrice: newPrice,
             ingridients: updatedIngredients
         });
+
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render() {
@@ -78,7 +97,8 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
-                    price={this.state.totalPrice} />
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable} />
             </Aux>
         );
     }
